0620a912a0e5fc7479ee0db11978cd981e4d75a8
Christian Kosman <christiankosman@gmail.com>
2017-10-08 09:12:46 -0700

    friendize

--- sendtocloud.js
+++ sendtocloud.js
@@ -1,11 +1,11 @@
 const CLOUD_PREFIX = "☁ ";
 
 var username = "";
-var connections = {};
+var connections = {};  //Array of WebSockets
 
 window.addEventListener('load', function() {
-	connect("178657884", 0, function() {});
-	connect("178581076", 1, function() {});
+	connect("178657884", 0, function() {}); //WebSocket 0, connects to first project
+	connect("178581076", 1, function() {}); //WebSocket 1, connects to second project
 });
 
 function transferValues() {
@@ -14,8 +14,8 @@ function transferValues() {
 	if(username.length > 0) {
 		msg.style.color = "yellow";
 		msg.innerHTML = "Syncing values...";
-		connections[0].getCloudVar("cloud_var", function(e) {
-			connections[1].setCloudVar("cloud_var", e.value);
+		connections[0].getCloudVar("cloud_var", function(e) { //Gets the value of cloud_var from project 0
+			connections[1].setCloudVar("cloud_var", e.value); //Sets the value of cloud_var in project 1
 			msg.innerHTML = "Values successfully synced";
 		});
 	} else {
@@ -28,7 +28,7 @@ function connect(pid, id, cb) {
 	connections[id] = new WebSocket("wss://clouddata.scratch.mit.edu/", [], {});
 	var varNameGet = "";
 	var cbGet = null;
-	connections[id].pid = pid;
+	connections[id].pid = pid; //pid = Project ID
 	connections[id].sendPacket = function(method, options) {
 		var obj = {
 			user: username,
@@ -38,29 +38,29 @@ function connect(pid, id, cb) {
 		for(var name in options) {
 			obj[name] = options[name];
 		}
-		this.send(JSON.stringify(obj) + "\n");
+		this.send(JSON.stringify(obj) + "\n"); //Send request through WebSocket
 	};
-	connections[id].setCloudVar = function(name, value) {
+	connections[id].setCloudVar = function(name, value) { //Sets a cloud variable
 		this.sendPacket('set', {
 			name: CLOUD_PREFIX + name,
 			value: "" + value
 		});
 	};
-	connections[id].getCloudVar = function(name, callback) {
+	connections[id].getCloudVar = function(name, callback) { //Gets a cloud variable
 		varNameGet = name;
 		if(callback) cbGet = callback;
 		this.sendPacket('handshake', {});
 	};
-	connections[id].onopen = function() {
+	connections[id].onopen = function() { //When we connect to the cloud server
 		console.log("Connected to cloud server, project id: " + pid);
 		this.sendPacket('handshake', {});
 		if(cb) cb();
 	};
-	connections[id].onclose = function() {
+	connections[id].onclose = function() { //When the conenction closes (mainly due to inactivity)
 		console.log("Connection closed on project " + pid + "... reconnecting");
-		connect(pid, id, cb);
+		connect(pid, id, cb); //Reconnect on close
 	};
-	connections[id].onmessage = function(e) {
+	connections[id].onmessage = function(e) { //Used for getting cloud var values
 		if(varNameGet !== "") {
 			var objs = e.data.split("\n");
 			var newJSONStr = "[";
